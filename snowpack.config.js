8c1253a8d2c736c2f7a94243eda6063c1afe4e2a
Pervez Iqbal <pervezfunctor@gmail.com>
2021-03-25 17:49:09 +0530

    initial commit

---
+++ snowpack.config.js
@@ -0,0 +1,39 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: { url: '/', static: true },
+    src: { url: '/dist' },
+  },
+  plugins: [
+    '@snowpack/plugin-react-refresh',
+    '@snowpack/plugin-dotenv',
+    '@snowpack/plugin-typescript',
+    [
+      '@canarise/snowpack-eslint-plugin',
+      {
+        globs: ['src/**/*.tsx', 'src/**/*.ts'], // You should provide this
+        options: {
+          errorOnUnmatchedPattern: false,
+        },
+        formatter: 'stylish',
+      },
+    ],
+  ],
+  routes: [
+    /* Enable an SPA Fallback in development: */
+    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+  ],
+  optimize: {
+    /* Example: Bundle your final build: */
+    // "bundle": true,
+  },
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+}
