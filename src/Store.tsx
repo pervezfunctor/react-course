e7a57fbbd5e2557cbd5d4ddbcddc2ea2146dc123
Pervez Iqbal <pervezfunctor@gmail.com>
2021-04-14 17:29:04 +0530

    improve selector for store

--- src/Store.tsx
+++ src/Store.tsx
@@ -1,3 +1,9 @@
+/* eslint-disable prefer-arrow/prefer-arrow-functions */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable functional/no-let */
+/* eslint-disable @typescript-eslint/naming-convention */
+
 import React, { Dispatch, Reducer, ReducerAction, ReducerState } from 'react'
 import { verify } from './safe'
 
@@ -6,7 +12,6 @@ export type StoreContext<R extends Reducer<any, any>> = readonly [
   Dispatch<ReducerAction<R>>,
 ]
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export function createStore<R extends Reducer<any, any>>(reducer: R) {
   const StoreContext = React.createContext<StoreContext<R> | undefined>(
     undefined,
@@ -28,14 +33,28 @@ export function createStore<R extends Reducer<any, any>>(reducer: R) {
     return context
   }
 
-  // eslint-disable-next-line prefer-arrow/prefer-arrow-functions
-  function useSelect<T>(selector: (state: ReducerState<R>) => T): T {
+  function useSelect<T, A>(
+    selector: (state: ReducerState<R>) => T,
+    compute: (x: T) => A,
+  ) {
+    const sel = React.useRef(selector).current
+    const cmp = React.useRef(compute).current
+
     const [state] = useStore()
-    return React.useMemo(() => selector(state), [selector, state])
+    let s = sel(state)
+    let result: any
+
+    return function <A>(): A {
+      const s2 = sel(state)
+      if (result === undefined || s !== s2) {
+        s = s2
+        result = cmp(s)
+      }
+      return result
+    }
   }
 
   return {
-    // eslint-disable-next-line @typescript-eslint/naming-convention
     Store,
     useStore,
     useSelect,
